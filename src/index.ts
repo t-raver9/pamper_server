39ff61f113ab2c1e6caab7bcdc589b3e9fd79863
Harrison Travers <harrisontravers@Harrisons-MacBook-Pro.local>
2023-08-05 11:11:40 +1000

    Added list user endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import { config } from "./config";
+import { PrismaClient } from "@prisma/client";
 
 const app: Express = express();
+const prisma = new PrismaClient();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Code with Harry. Ready to run on Heroku.");
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Error occurred while retrieving users" });
+  }
 });
 
 app.listen(config.server.port, () => {
